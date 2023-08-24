fa519a812158e564be86034f7883a9616ad413a6
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-08-24 01:47:24 +0000

    push to stack and pall from the stack

---
+++ push-pall.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+
+/**
+ * push - push elements to the stack.
+ * @stack: doubly linked list
+ * @line_number: number of line.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *argmnt;
+	stack_t *newn;
+	int val;
+
+	argmnt = strtok(NULL, " \t\n");
+	if (!argmnt)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	val = atoi(argmnt);
+
+	if (val == 0 && strcmp(argmnt, "0") != 0)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	newn = malloc(sizeof(stack_t));
+	if (newn == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newn->n = val;
+	newn->prev = NULL;
+	newn->next = *stack;
+
+	if (*stack)
+		(*stack)->prev = newn;
+	*stack = newn;
+}
+/**
+ * pall - print or display elements in the stack.
+ * @stack: doubly linked list.
+ */
+void pall(stack_t **stack)
+{
+	stack_t *currt;
+
+	currt = *stack;
+	while (currt)
+	{
+		printf("%d\n", currt->n);
+		currt = currt->next;
+	}
+}

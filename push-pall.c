be44ed8bded52cea35e16acdb077e42b55d80ddc
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-08-26 21:26:47 +0000

    betty

--- push-pall.c
+++ push-pall.c
@@ -18,7 +18,6 @@ void push(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
 	val = atoi(argmnt);
 
 	newn = malloc(sizeof(stack_t));
